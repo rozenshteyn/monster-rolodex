81b6faba5f3e135713e8e697a280f05214a72580
Roman Rozenshteyn <rozenshteyn@yahoo.com>
2021-05-02 15:08:05 -0700

    Fixed failing App.test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { container } = render(<App />);
-  expect(container.firstChild).toHaveClass("card-container");
+test('renders app header', () => {
+  render(<App />);
+  const headerElement = screen.getByText(/Monster rolodex/i);
+  expect(headerElement).toBeInTheDocument();
+
 });
